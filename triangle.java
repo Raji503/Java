75b31f0f1619e9b0a7f5c8eb230138c6ac2dcba3
Raji503 <37294053+Raji503@users.noreply.github.com>
2018-03-13 16:46:41 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,26 @@
+public class triangle
+{
+	public static void main(String args[])
+	{	
+		int i=0;
+		while(i<=3)
+		{
+			if(i==0)
+			{
+			System.out.println("*");
+			}
+			else if(i==1)
+			{
+			System.out.println("*" +"\t" +"*");
+			}
+			else if(i==2)
+			{
+			System.out.println("*" +"\t" +"*" +"\t" +"*");
+			}
+			
+		i++;
+		}
+
+
+	}
+}
