22b4b0bd9d58cb23368e1ceab8ba99713bd25850
Raji503 <37294053+Raji503@users.noreply.github.com>
2018-03-14 14:58:45 +0530

    Add files via upload

---
+++ noincretriangle.java
@@ -0,0 +1,17 @@
+public class noincretriangle
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				
+				System.out.print(j+i);
+				
+			}	
+			System.out.print("\n");
+		}
+		
+	}
+}
