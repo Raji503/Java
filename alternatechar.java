dec07320902454d64a8b1c651a51cf2b2fc2f31e
Raji503 <37294053+Raji503@users.noreply.github.com>
2018-03-23 22:40:53 +0530

    Update alternatechar.java

--- alternatechar.java
+++ alternatechar.java
@@ -1,34 +1,55 @@
 import java.util.Scanner;
 
 public class alternatechar
-{
+{	
+
+	static int len;
+	
 	public static void main(String args[])
 	{
 		
 		String str;
+
 		System.out.println("Enter the string: ");
 		
 		Scanner sc=new Scanner(System.in);
 		str=sc.nextLine();
 
-		System.out.println(str);
+		System.out.println("\n" +"The given string is: " +str +"\n");
 		
 		String str1[]=str.split("\\s");
 		
+		System.out.println("After using split: ");
 		
-
 		for(int i=0;i<str1.length;i++)
 		{
+			
 			System.out.println(str1[i]);
+
 		}
+
+		System.out.println("\n" +"Printing alternate characters: ");
+		
 		
 		for(int j=0;j<str1.length;j++)
 		{
 
-			int len = str1[j].length();
-			System.out.println(len);
+			len = str1[j].length();
+
+			//System.out.println(len);
+
+			for(int i=0;i<len;i=i+2)
+			{
+
+				char ch[]=str1[j].toCharArray();
+
+				System.out.print(ch[i] +"  ");
+
+			}
+
 		
-		}
+		}			
+		
 
 	}
 
